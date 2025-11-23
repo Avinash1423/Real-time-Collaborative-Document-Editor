ca9a7261a391acec161921ad88a46836bfc4c961
Avinash Niyas <avinashNiyaz1423@gmail.com>
2025-11-24 00:00:13 +0530

    /ws

--- docx/src/Editor.jsx
+++ docx/src/Editor.jsx
@@ -54,7 +54,7 @@ console.log("🔵 Creating SockJS socket...");
  // stompClient=Stomp.over(socket); deprecated
 
    const stompClient=new Client({
-   brokerURL:"/ws/",
+   brokerURL:"/ws",
    // webSocketFactory:()=>socket,
    debug:(str)=>{console.log("STOMP DEBUG:", str)},
 
